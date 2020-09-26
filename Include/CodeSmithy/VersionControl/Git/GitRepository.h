5a6bcc37baabf0037d51293b26d51ec3d12ec576
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2020-09-26 23:06:34 +0100

    Updates

---
+++ Include/CodeSmithy/VersionControl/Git/GitRepository.h
@@ -0,0 +1,48 @@
+/*
+    Copyright (c) 2017-2020 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/CodeSmithyIDE/VersionControl/blob/master/LICENSE.txt
+*/
+
+#ifndef _CODESMITHY_VERSIONCONTROL_GIT_GITREPOSITORY_H_
+#define _CODESMITHY_VERSIONCONTROL_GIT_GITREPOSITORY_H_
+
+#include "CodeSmithy/Tasks/Task.h"
+#include <git2.h>
+#include <string>
+
+namespace CodeSmithy
+{
+
+/**
+    This class is a wrapper around libgit2 to interact
+    with Git repositories.
+*/
+class GitRepository
+{
+public:
+    GitRepository();
+    ~GitRepository();
+
+    std::unique_ptr<Task> init(const std::string& path);
+    // TODO url can actually be a file path, but in that case the clone may have 
+    // hard links to the cloned repo, that may be some behavior I want to change
+    std::unique_ptr<Task> clone(const std::string& url, const std::string& clonePath);
+    std::unique_ptr<Task> open(const std::string& path);
+
+    void checkout();
+    void branch();
+
+    void fetch();
+    void merge();
+    void pull();
+
+    bool checkIfRepository(const std::string& path) const;
+
+private:
+    git_repository* m_repository;
+};
+
+}
+
+#endif
