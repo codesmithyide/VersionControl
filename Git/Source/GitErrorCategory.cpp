74f9ddb82d71aa5702c3e7dd6308b3e92cea839c
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2020-10-16 18:31:23 +0100

    Add files

---
+++ Git/Source/GitErrorCategory.cpp
@@ -0,0 +1,7 @@
+/*
+    Copyright (c) 2020 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/CodeSmithyIDE/VersionControl/blob/master/LICENSE.txt
+*/
+
+#include "GitErrorCategory.h"
---
+++ Include/CodeSmithy/VersionControl/Git/GitTasks.h
@@ -0,0 +1,10 @@
+/*
+    Copyright (c) 2020 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/CodeSmithyIDE/VersionControl/blob/master/LICENSE.txt
+*/
+
+#ifndef _CODESMITHY_VERSIONCONTROL_GIT_GITTASKS_H_
+#define _CODESMITHY_VERSIONCONTROL_GIT_GITTASKS_H_
+
+#endif
